c3c35e47af0838ffe5dd9468bc92181933ac6c68
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-07 14:57:14 -0700

    Create test 2 and corresponding initialize method

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,7 @@
 class Board
+  attr_reader :layout
   
+  def initialize
+    @layout = Hash.new
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,8 +7,12 @@ describe 'Board' do
       @board = Board.new
     end
 
-    it 'exists' do
+    it '1. exists' do
       expect(@board).to be_a(Board)
+    end
+
+    it '2. has readable attributes' do
+      expect(@board.layout).to be_a(Hash)
     end
   end
 end
