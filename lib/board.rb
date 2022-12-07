e5c0483095c9252c604c320c6011eecc979a0444
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-07 16:25:53 -0700

    Create test 3 and working 'build_board' method

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,35 @@
 class Board
   attr_reader :layout
-  
+
   def initialize
-    @layout = Hash.new
+    @layout = build_board
+  end
+
+  def build_board
+    new_board = {}
+    row = 0
+    column = ""
+    key = ""
+    value = []
+
+    #Creates 7 columns
+    7.times do |i|
+      column = ["A", "B", "C", "D", "E", "F", "G"]
+
+      #Creates 6 rows
+      6.times do |j|
+        #Determine rows
+        row = j + 1
+
+        #Creates the keys and values for new_board hash
+        key = column[i] + row.to_s
+        value = [i + 1, row, nil]
+
+        #Putting key => value pair into new_board hash
+        new_board[key.to_sym] = value 
+
+      end
+    end
+    new_board
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,12 +7,28 @@ describe 'Board' do
       @board = Board.new
     end
 
-    it '1. exists' do
+    it '1. Exists' do
       expect(@board).to be_a(Board)
     end
 
-    it '2. has readable attributes' do
+    it '2. Has readable attributes' do
       expect(@board.layout).to be_a(Hash)
+    end
+
+    it '3. Can build board' do
+      expect(@board.layout.length).to eq(42)
+
+      num_columns = @board.layout.select do |key, value|
+        value[1] == 6
+      end
+
+      expect(num_columns.length).to be(7)
+
+      num_rows = @board.layout.select do |key, value|
+        value[0] == 3
+      end
+
+      expect(num_rows.length).to be(6)
     end
   end
 end
