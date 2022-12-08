4be48ef9b1c9490ce525a976afca90a58ca7ce8a
Adam Bailey <ab67319@gmail.com>
2022-12-07 21:26:32 -0700

    Create '2. has readable attributes' example test and initialize method with attr_reader

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+class Game
+    attr_reader :board
+
+    def initialize
+        @board = {}
+    end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,16 @@
 require './lib/game'
 
+
 describe Game do
     before(:each) do 
         @game = Game.new
     end
     
     it '1. Can create an instance' do
+        expect(@game).to be_a(Game)
+    end
 
+    it '2. has readable attributes' do
+        expect(@game.board).to eq({})
     end
 end
