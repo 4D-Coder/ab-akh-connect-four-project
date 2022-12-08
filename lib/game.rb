e70c6e78e83c11b8a4f5b817c34f1dcec88a50d2
Adam Bailey <ab67319@gmail.com>
2022-12-08 14:02:58 -0700

    Create '4. can print board' example test and print_board method

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,39 @@ class Game
     attr_reader :board
 
     def initialize
-        @board = {}
+        @board = Board.new
     end
 
     def start
         puts welcome_user
+        puts print_board
     end
 
     def welcome_user
        "Welcome to CONNECT FOUR"
+    end
+
+    def print_board
+        board_string = "ABCDEFG\n"
+
+        6.times do
+            counter = 6
+    
+            row_string = @board.layout.select do |cell|
+                @board.layout[cell][:row] == counter
+            end
+            
+            row_string.each do |cell|
+                if cell[1][:checker] != nil
+                    board_string << cell[1][:checker].type
+                else
+                    board_string << "."
+                end
+            end
+    
+            board_string << "\n"
+            counter -= 1
+        end
+        return board_string
     end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,10 +11,19 @@ describe Game do
     end
 
     it '2. has readable attributes' do
-        expect(@game.board).to eq({})
+        expect(@game.board).to be_a({})
     end
 
     it '3. can get welcome message' do
         expect(@game.welcome_user).to eq("Welcome to CONNECT FOUR")
     end
+
+    it '4. can print board' do
+        expect(@game.print_board).to eq("ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......\n")
+    end
 end
+
+require './lib/game'
+require './lib/board'
+require './lib/checker'
+game = Game.new
