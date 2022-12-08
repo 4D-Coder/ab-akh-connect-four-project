efca21ec3739f7084a8026e906522d65c7038ba0
Adam Bailey <ab67319@gmail.com>
2022-12-08 16:53:32 -0700

    Create example 2., initialize method and add attr_reader variables

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,11 +3,16 @@ require './lib/player'
 describe Player do
     context 'Iteration 2' do
         before(:each) do
-            @player = Player.new
+            @player = Player.new("Player", "X")
         end
 
         it '1. Can create an instance' do
             expect(@player).to be_a(Player)
+        end
+
+        it '2. Has readable attributes' do
+            expect(@player.checker).to eq("X")
+            expect(@player.name).to eq("Player")
         end
     end
 end
