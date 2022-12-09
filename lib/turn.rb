daf65037323b96fa787031e733cf9c749e62d2ce
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-09 15:48:57 -0700

    Build working method for test 3

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,23 @@
 require './lib/board'
 
 class Turn 
-  attr_reader :board
+  attr_reader :board, :turn
 
   def initialize
+    @turn = turn
     @board = Board.new
   end
 
+  def board_full?
+    full_board = 42
+    @board.layout.each do |cell, attr|
+      if attr[:checker] != nil
+        full_board -= 1
+      else
+        break
+      end
+    end  
+    
+    full_board == 0
+  end
 end
