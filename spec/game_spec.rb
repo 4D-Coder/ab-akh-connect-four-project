c195d7ff8390ba7a9b18adcf3eae9b7a42d9165e
Adam Bailey <ab67319@gmail.com>
2022-12-10 19:48:50 -0700

    Separate iteration 1 and 2 describe patterns

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,23 +3,32 @@ require './lib/board'
 
 
 describe Game do
-    before(:each) do 
-        @game = Game.new
-    end
-    
-    it '1. Can create an instance' do
-        expect(@game).to be_a(Game)
+    describe 'Iteration 1' do
+        before(:each) do 
+            @game = Game.new
+        end
+        
+        it '1. Can create an instance' do
+            expect(@game).to be_a(Game)
+        end
+
+        it '2. has readable attributes' do
+            expect(@game.board).to be_a(Board)
+        end
+
+        it '3. can get welcome message' do
+            expect(@game.welcome_user).to eq("Welcome to CONNECT FOUR")
+        end
+
+        it '4. can print board' do
+            expect(@game.print_board).to eq("ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......\n")
+        end
     end
 
-    it '2. has readable attributes' do
-        expect(@game.board).to be_a(Board)
-    end
-
-    it '3. can get welcome message' do
-        expect(@game.welcome_user).to eq("Welcome to CONNECT FOUR")
-    end
-
-    it '4. can print board' do
-        expect(@game.print_board).to eq("ABCDEFG\n.......\n.......\n.......\n.......\n.......\n.......\n")
+    describe 'Iteration 2' do
+        before(:each) do 
+            @game = Game.new
+        end
+        
     end
 end
