cdf1cd965ffd81752f336a7786e248ef63dab5b2
Adam Bailey <ab67319@gmail.com>
2022-12-10 20:39:06 -0700

    Create start class and refactor counter on print board method

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,12 @@
 class Game
-    attr_reader :board
+    attr_reader :board,
+                :player,
+                :computer
 
     def initialize
         @board = Board.new
+        @player = Player.new("Player", "X")
+        @computer = Player.new("Computer", "O")
     end
 
     def start
@@ -17,16 +21,16 @@ class Game
     def print_board
         # Create board string accumulator 
         board_string = "ABCDEFG\n"
+        counter = 6
 
         # Add each board cell's checker value to string in reverse row order, starting at 6
         6.times do
-            counter = 6
             
             # Create a hash of each key:value pair of the row corresponding to the counter
             row_string = @board.layout.select do |cell|
                 @board.layout[cell][:row] == counter
             end
-            
+            #require "pry"; binding.pry
             # Add each checker piece or '.' to the string accumulator
             row_string.each do |cell|
                 if cell[1][:checker] != nil
@@ -43,4 +47,26 @@ class Game
         end
         board_string
     end
+
+    def start
+        puts print_board
+
+        turn = Turn.new(@board, @player)
+    
+        until turn.board_full?
+            turn = Turn.new(@board, @player)
+            turn.play_turn
+            puts print_board
+            puts
+    
+            turn = Turn.new(@board, @computer)
+            turn.play_turn
+            puts print_board
+            puts
+        end
+
+       "The game was a DRAW, you couldn't beat a computer choosing at random"
+    end
+
+    
 end
